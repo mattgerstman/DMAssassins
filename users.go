1203a644da6c25362aa825dc437643c19a9cafb2
Matt Gerstman <mattgerstman@gmail.com>
2014-06-25 20:22:28 -0700

    Users model

---
+++ users.go
@@ -0,0 +1,60 @@
+package main
+
+import {
+	"code.google.com/p/go-uuid/uuid"
+	"code.google.com/p/go.crypto/bcrypt"
+
+}
+
+var db *sql.DB
+
+type User struct {
+	user_id string
+	email string
+	secret string
+}
+
+func connect() {
+	db, err = sql.Open("postgres", "postgres://localhost?dbname=dmassassins&sslmode=disable")
+}
+
+func clear(b []byte) {
+    for i := 0; i < len(b); i++ {
+        b[i] = 0;
+    }
+}
+
+func Crypt(password []byte) ([]byte, error) {
+    defer clear(password)
+    return bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+}
+
+
+func AddPlayer(email string, plainPW string, secret string) User *{
+
+	id := uuid.New()
+	password, err := Crypt([]byte(strings.TrimSpace(plainPW)))
+	
+	_, err := db.Query(`INSERT INTO dm_users (user_id, email, password, secret) VALUES ($1,$2,$3,$4)`, id, email, password, secret)
+	fmt.Println(err)
+	returnUser := &User{id, email, secret}
+	return returnUser;
+}
+
+func KillTarget(user User, pin int) {
+	
+	logged_in_user := user.user_id
+
+	row := db.QueryRow(`SELECT pin FROM dm_users WHERE user_id = (SELECT target_id FROM dm_user_targets where user_id = $1)`, logged_in_user)
+	if pin == target_pin {
+		
+		row = db.QueryRow(`SELECT target_id FROM dm_user_targets WHERE user_id = (SELECT target_id FROM dm_user_targets where user_id = $1)`, logged_in_user)
+		
+		var new_target_id string
+		row.Scan(&new_target_id)
+		row = db.QueryRow(`DELETE FROM dm_user_targets WHERE user_id = (SELECT target_id from dm_user_targets WHERE user_id = $1)`, logged_in_user)
+		row = db.QueryRow(`UPDATE dm_user_targets SET target_id = $1 WHERE user_id = $2`, new_target_id, logged_in_user)
+	} else {
+		fmt.Println("Invalid pin\n", target_pin, "\n")
+	}
+}
