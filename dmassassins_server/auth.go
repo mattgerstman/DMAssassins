b27e669191020bfb1396d99fad493ed645d0a0dc
Matt Gerstman <mattgerstman@gmail.com>
2014-08-17 10:17:51 -0700

    Issue #7, User Role Improvements
    Properly validates user role, added super admin role (me) to manage any
    game I want

--- dmassassins_server/auth.go
+++ dmassassins_server/auth.go
@@ -10,9 +10,10 @@ import (
 )
 
 const (
-	RoleAdmin   = 3
-	RoleCaptain = 2
-	RoleUser    = 1
+	RoleSuperAdmin = 4
+	RoleAdmin      = 3
+	RoleCaptain    = 2
+	RoleUser       = 1
 )
 
 func GetBasicAuth(r *http.Request) (userId uuid.UUID, token string, appErr *ApplicationError) {
@@ -62,35 +63,57 @@ func GetBasicAuth(r *http.Request) (userId uuid.UUID, token string, appErr *Appl
 	return userId, token, nil
 }
 
-func compareRole(role string, roleId int) bool {
+func compareRole(role string, roleId int) (greaterThanOrEqualTo bool) {
 	var roles = map[string]int{
-		"dm_admin":   RoleAdmin,
-		"dm_captain": RoleCaptain,
-		"dm_user":    RoleUser,
+		"dm_super_admin": RoleSuperAdmin,
+		"dm_admin":       RoleAdmin,
+		"dm_captain":     RoleCaptain,
+		"dm_user":        RoleUser,
 	}
 	return roles[role] >= roleId
 }
 
-func RequiresUser(r *http.Request) (bool, *ApplicationError) {
-	role, appErr := getRoleFromHeaders(r)
+func RequiresUser(r *http.Request, reqTeamId, reqUserId uuid.UUID) (isRightUser bool, appErr *ApplicationError) {
+	role, teamId, userId, appErr := getRoleFromRequest(r)
 	if appErr != nil {
 		return false, appErr
 	}
-	return compareRole(role, RoleUser), nil
+
+	if (uuid.Equal(userId, reqUserId)) || (reqUserId == nil) {
+		return true, nil
+	}
+
+	return isTeamCaptain(role, teamId, reqTeamId), nil
 
 }
 
-func RequiresCaptain(r *http.Request) (bool, *ApplicationError) {
-	role, appErr := getRoleFromHeaders(r)
+func isTeamCaptain(role string, teamId, reqTeamId uuid.UUID) (isRightCaptain bool) {
+
+	if compareRole(role, RoleAdmin) {
+		return true
+	}
+
+	if !compareRole(role, RoleCaptain) {
+		return false
+	}
+	if (uuid.Equal(teamId, reqTeamId)) || (reqTeamId == nil) {
+		return true
+	}
+	return false
+
+}
+
+func RequiresCaptain(r *http.Request, reqTeamId uuid.UUID) (isRightCaptain bool, appErr *ApplicationError) {
+	role, teamId, _, appErr := getRoleFromRequest(r)
 	if appErr != nil {
 		return false, appErr
 	}
-	return compareRole(role, RoleCaptain), nil
+	return isTeamCaptain(role, teamId, reqTeamId), nil
 
 }
 
-func RequiresAdmin(r *http.Request) (bool, *ApplicationError) {
-	role, appErr := getRoleFromHeaders(r)
+func RequiresAdmin(r *http.Request) (isRightAdmin bool, appErr *ApplicationError) {
+	role, _, _, appErr := getRoleFromRequest(r)
 	if appErr != nil {
 		return false, appErr
 	}
@@ -98,28 +121,28 @@ func RequiresAdmin(r *http.Request) (bool, *ApplicationError) {
 
 }
 
-func getRoleFromHeaders(r *http.Request) (string, *ApplicationError) {
+func getRoleFromRequest(r *http.Request) (userRole string, teamId uuid.UUID, userId uuid.UUID, appErr *ApplicationError) {
 	userId, token, appErr := GetBasicAuth(r)
 	if appErr != nil {
-		return "", appErr
+		return "", nil, nil, appErr
 	}
 	vars := mux.Vars(r)
 	gameId := uuid.Parse(vars["game_id"])
 
-	var facebookId, facebookToken, userRole string
-	err := db.QueryRow(`SELECT user.facebook_id, user.facebook_token, game.user_role FROM dm_users AS user, dm_user_game_mapping AS game WHERE user.user_id = game.user_id AND game.user_id = $1 AND game.game_id = $2`, userId.String(), gameId.String()).Scan(&facebookId, &facebookToken, &userRole)
+	var facebookId, facebookToken, teamIdBuffer string
+	err := db.QueryRow(`SELECT user.facebook_id, user.facebook_token, game.user_role, game.team_id FROM dm_users AS user, dm_user_game_mapping AS game WHERE user.user_id = game.user_id AND game.user_id = $1 AND (game.game_id = $2 OR user.user_role == 'dm_super_admin')`, userId.String(), gameId.String()).Scan(&facebookId, &facebookToken, &userRole, &teamIdBuffer)
 	if err != nil {
-		return "", NewApplicationError("Internal Error", err, ErrCodeDatabase)
+		return "", nil, nil, NewApplicationError("Internal Error", err, ErrCodeDatabase)
 	}
-
+	teamId = uuid.Parse(teamIdBuffer)
 	if facebookToken != token {
 		apiFacebookId, appErr := GetFacebookIdFromToken(token)
 		if appErr != nil {
-			return "", appErr
+			return "", nil, nil, appErr
 		}
 		if apiFacebookId != facebookId {
-			return "", NewApplicationError("Invalid Token", err, ErrCodeInvalidFBToken)
+			return "", nil, nil, NewApplicationError("Invalid Token", err, ErrCodeInvalidFBToken)
 		}
 	}
-	return userRole, nil
+	return userRole, teamId, userId, nil
 }
