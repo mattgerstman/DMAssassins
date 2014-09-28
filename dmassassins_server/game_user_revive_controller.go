9160c9ac32fc48101049acdce222ddd4e760f3e8
mattgerstman <mattgerstman@gmail.com>
2014-09-27 23:21:44 -0700

    Major improvements to user management

---
+++ dmassassins_server/game_user_revive_controller.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"code.google.com/p/go-uuid/uuid"
+	"errors"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+// POST - Revives a user and places them in between an assassin target apir
+func postGameUserRevive(r *http.Request) (appErr *ApplicationError) {
+	appErr = RequiresAdmin(r)
+	if appErr != nil {
+		return appErr
+	}
+	vars := mux.Vars(r)
+	userId := uuid.Parse(vars["user_id"])
+	if userId == nil {
+		msg := "Invalid UUID: user_id " + vars["user_id"]
+		err := errors.New(msg)
+		return NewApplicationError(msg, err, ErrCodeInvalidUUID)
+	}
+
+	gameId := uuid.Parse(vars["game_id"])
+	if gameId == nil {
+		msg := "Invalid UUID: game_id " + vars["game_id"]
+		err := errors.New(msg)
+		return NewApplicationError(msg, err, ErrCodeInvalidUUID)
+	}
+
+	gameMapping, appErr := GetGameMapping(userId, gameId)
+	if appErr != nil {
+		return appErr
+	}
+
+	return gameMapping.Revive()
+}
+
+// Handler for /game path
+func GameUserReviveHandler() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		var obj interface{}
+		var err *ApplicationError
+
+		switch r.Method {
+		case "POST":
+			err = postGameUserRevive(r)
+
+		default:
+			obj = nil
+			msg := "Not Found"
+			tempErr := errors.New("Invalid Http Method")
+			err = NewApplicationError(msg, tempErr, ErrCodeNotFoundMethod)
+		}
+		WriteObjToPayload(w, r, obj, err)
+	}
+}
